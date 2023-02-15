0d726550855138887a404adbc9a6bba274bb639c
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-02-15 20:42:16 +0100

    Update LoadingUtil.java

--- app/src/main/java/com/mayank7319gmail/hospitallocator/utils/LoadingUtil.java
+++ app/src/main/java/com/mayank7319gmail/hospitallocator/utils/LoadingUtil.java
@@ -1,11 +1,8 @@
-package com.mayank7319gmail.hospitallocator.utils;
+package com.heistejiri.hospitallocator.utils;
 
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by Mayank Gupta on 25-12-2017.
- */
 
 public class LoadingUtil {
     public static void enableDisableView(View view, boolean enabled) {
